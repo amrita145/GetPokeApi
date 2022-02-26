410de615d253481a52cad1bcfe2aebc92f3c50ce
amrita <amritarajhibm111@gmail.com>
2022-02-26 17:09:59 +0530

    Updates

---
+++ precache-manifest.c4a84dd8ffb23b8ebbc2cef8832bb147.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5911270e5bd10db6289c3b4143252bb8",
+    "url": "/GetPokeApi/index.html"
+  },
+  {
+    "revision": "3fce6f46e42202d4b590",
+    "url": "/GetPokeApi/static/css/main.47f666f0.chunk.css"
+  },
+  {
+    "revision": "87c3aeaa0f6960155e16",
+    "url": "/GetPokeApi/static/js/2.d09f8bdb.chunk.js"
+  },
+  {
+    "revision": "3fce6f46e42202d4b590",
+    "url": "/GetPokeApi/static/js/main.1395bf35.chunk.js"
+  },
+  {
+    "revision": "7d108a320b8e6e408e49",
+    "url": "/GetPokeApi/static/js/runtime~main.299365b6.js"
+  }
+]);
